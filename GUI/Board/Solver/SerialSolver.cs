b2d74e3a4239d3c372f93edf5896397c751be045
Sergey Zhezhel <just.ru@live.com>
2015-08-08 11:21:11 +0300

    add missing files

---
+++ GUI/Board/Solver/SerialSolver.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Solver
+{
+	public class SerialSolver : ISolver
+    {
+	    public ExecutionResult[] Solve(ExecutionRequest request)
+	    {
+		    return Calculate(request.Snapshot, null, request.Options.MaxWidth, request.Options.MaxHeight).ToArray();
+	    }
+
+	    private static IEnumerable<ExecutionResult> Calculate(
+			Snapshot baseSnapshot, MoveDirection? move, int maxWidth, int depth)
+	    {
+			var snapshot = move.HasValue ? baseSnapshot.New(move.Value) : baseSnapshot;
+			// stop recursion
+			if (snapshot.Finished || depth <= 0)
+		    {
+			    yield return new ExecutionResult
+			    {
+				    Commands = new MoveDirection[0],
+				    Estimation = SnapshotEvaluate(snapshot)
+			    };
+				yield break;
+		    }
+			// TODO: check for illegal moves (e.g. E-W)
+			var candidateMoves = (MoveDirection[])Enum.GetValues(typeof (MoveDirection));
+			var childResults = candidateMoves
+				.SelectMany(m => Calculate(snapshot, m, maxWidth, depth - 1)) // recursion
+				.OrderByDescending(r => r.Estimation)
+				.Take(maxWidth);
+		    foreach (var result in childResults)
+		    {
+				var estimate = SnapshotEvaluate(snapshot);
+				yield return new ExecutionResult
+				{
+					Commands = move.HasValue
+						? result.Commands.Prepend(move.Value).ToArray()
+						: result.Commands,
+					Estimation = estimate,
+				};
+		    }
+	    }
+
+	    private static double SnapshotEvaluate(Snapshot snapshot, params MoveDirection[] nextMoves)
+	    {
+		    int depth = snapshot.Field.Height - snapshot.CurrentUnit.Members.Min(p => p.Y);
+			return snapshot.Score - depth + nextMoves.Length;
+	    }
+    }
+}
---
+++ GUI/Board/Solver/Snapshot.cs
@@ -0,0 +1,68 @@
+namespace Solver
+{
+	public sealed class Snapshot
+	{
+		public Field Field { get; set; }
+
+		public Unit CurrentUnit { get; set; }
+
+		public UnitsQueue UnitsQueue { get; set; }
+
+		public int Score { get; set; }
+
+		public bool Finished { get; set; }
+
+		public int UnitIndex { get; set; }
+
+		private Snapshot(Snapshot other)
+		{
+			CurrentUnit = other.CurrentUnit;
+			Field = new Field(other.Field);
+			Finished = other.Finished;
+			Score = other.Score;
+			UnitIndex = other.UnitIndex;
+			// TODO: move index from queue to this class
+			UnitsQueue = other.UnitsQueue;
+		}
+
+		public Snapshot(Input input, uint seed)
+		{
+			Field = new Field(input);
+			UnitsQueue = new UnitsQueue(input.Units, seed, input.SourceLength);
+			CurrentUnit = Field.Spawn(UnitsQueue.Get(0));
+			Score = 0;
+		}
+
+		public Snapshot New(MoveDirection move)
+		{
+			var snapshot = new Snapshot(this); // clone
+			var unit = CurrentUnit.Translate(move);
+			if (Collisions.GetCollision(Field, unit) != CollisionType.None)
+			{
+				foreach (var pos in CurrentUnit.Members)
+				{
+					snapshot.Field[pos.X, pos.Y] = true; // lock position
+				}
+				// TODO: clear row, update cleared rows count, move rows down!
+				snapshot.UnitIndex++;
+				if (snapshot.UnitIndex >= snapshot.UnitsQueue.MaxUnits)
+				{
+					snapshot.Finished = true;
+				}
+				else
+				{
+					var nextUnit = Field.Spawn(UnitsQueue.Get(snapshot.UnitIndex));
+					if (Collisions.GetCollision(Field, nextUnit) != CollisionType.None)
+					{
+						snapshot.Finished = true;
+					}
+					else
+					{
+						snapshot.CurrentUnit = nextUnit;
+					}
+				}
+			}
+			return snapshot;
+		}
+	}
+}
