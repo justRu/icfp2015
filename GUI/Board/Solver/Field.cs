d7e743b64977025fff23c67785e25c834e3108f0
GorisWW <gorisww@gmail.com>
2015-08-08 14:59:30 +0300

    clearing filled rows

--- GUI/Board/Solver/Field.cs
+++ GUI/Board/Solver/Field.cs
@@ -1,60 +1,101 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Solver
 {
-	public sealed class Field
-	{
-		public byte[] Cells { get; set; } // TODO: make em Int32 or Int64
+    public sealed class Field
+    {
+        public byte[] Cells { get; set; } // TODO: make em Int32 or Int64
 
-		public int Width { get; set; }
+        public int Width { get; set; }
 
-		public int Height { get; set; }
+        public int Height { get; set; }
 
-		public Field(Field other)
-		{
-			Width = other.Width;
-			Height = other.Height;
-			Cells = other.Cells.ToArray();
-		}
+        public Field(Field other)
+        {
+            Width = other.Width;
+            Height = other.Height;
+            Cells = other.Cells.ToArray();
+        }
 
-		public Field(Input input)
-		{
-			Width = input.Width;
-			Height = input.Height;
-			Cells = new byte[(uint)Math.Ceiling(((double)Width * Height) / 8)];
-			foreach (var cell in input.Filled)
-			{
-				this[cell.X, cell.Y] = true;
-			}
-		}
+        public Field(Input input)
+        {
+            Width = input.Width;
+            Height = input.Height;
+            Cells = new byte[(uint)Math.Ceiling(((double)Width * Height) / 8)];
+            foreach (var cell in input.Filled)
+            {
+                this[cell.X, cell.Y] = true;
+            }
+        }
 
-		public bool this[int x, int y]
-		{
-			get { return this[x + y * Width]; }
-			set { this[x + y * Width] = value; }
-		}
+        public bool this[int x, int y]
+        {
+            get { return this[x + y * Width]; }
+            set { this[x + y * Width] = value; }
+        }
 
-		public bool IsLineFull(int y)
-		{
-			for (int i = 0; i < Width; i++)
-			{
-				if (!this[i, y])
-					return false;
-			}
-			return true;
-		}
+        public bool IsLineFull(int y)
+        {
+            for (int i = 0; i < Width; i++)
+            {
+                if (!this[i, y])
+                    return false;
+            }
+            return true;
+        }
 
-		private bool this[int offset]
-		{
-			get { return (Cells[offset >> 3] & (1 << (offset & 7))) != 0; }
-			set
-			{
-				if (value)
-					Cells[offset >> 3] |= (byte)(1 << (offset & 7));
-				else
-					Cells[offset >> 3] &= (byte)~(1 << (offset & 7));
-			}
-		}
-	}
+        private bool this[int offset]
+        {
+            get { return (Cells[offset >> 3] & (1 << (offset & 7))) != 0; }
+            set
+            {
+                if (value)
+                    Cells[offset >> 3] |= (byte)(1 << (offset & 7));
+                else
+                    Cells[offset >> 3] &= (byte)~(1 << (offset & 7));
+            }
+        }
+
+        const byte One = 1;
+        public IEnumerable<bool> GetEnumerable()
+        {
+            
+            int maxCount = Width * Height;
+            foreach (var cell in Cells)
+            {
+                byte shifted = 1;
+                for (int shift = 0; shift < 8; shift++)
+                {
+                    yield return (cell & shifted) != 0;
+                    maxCount--;
+                    if (maxCount == 0)
+                        yield break;
+                    shifted = unchecked((byte)(shifted << One));
+                }
+            }
+        }
+
+        public void SaveEnumerable(IEnumerable<bool> input)
+        {
+            //clear
+            Cells = new byte[Cells.Length];
+            var enumerator = input.GetEnumerator();
+            for(int i =0; i< Cells.Length;i++) 
+            {
+                byte shifted = 1;
+                for (int shift = 0; shift < 8; shift++)
+                {
+                    if (!enumerator.MoveNext())
+                        return;
+                    if(enumerator.Current)
+                        Cells[i] |= shifted;
+                    shifted = unchecked((byte)(shifted << One));
+                }
+            }
+        }
+
+
+    }
 }
--- GUI/Board/Solver/Game.cs
+++ GUI/Board/Solver/Game.cs
@@ -1,59 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Solver
 {
-	public static class Game
-	{
-		public static Snapshot MakeMove(Snapshot prevSnapshot, MoveDirection move)
-		{
-			var snapshot = new Snapshot(prevSnapshot); // clone
-			var unit = snapshot.CurrentUnit.Translate(move);
-			if (Collisions.GetCollision(snapshot.Field, unit) != CollisionType.None)
-			{
-				LockUnit(snapshot, snapshot.CurrentUnit);
-			}
-			else
-			{
-				snapshot.CurrentUnit = unit;
-			}
-			return snapshot;
-		}
+    public static class Game
+    {
+        public static Snapshot MakeMove(Snapshot prevSnapshot, MoveDirection move)
+        {
+            var snapshot = new Snapshot(prevSnapshot); // clone
+            var unit = snapshot.CurrentUnit.Translate(move);
+            if (Collisions.GetCollision(snapshot.Field, unit) != CollisionType.None)
+            {
+                LockUnit(snapshot, snapshot.CurrentUnit);
+            }
+            else
+            {
+                snapshot.CurrentUnit = unit;
+            }
+            return snapshot;
+        }
 
-		private static void LockUnit(Snapshot snapshot, Unit unit)
-		{
-			foreach (var pos in unit.Members)
-			{
-				snapshot.Field[pos.X, pos.Y] = true; // lock position
-			}
-			DeleteLines(snapshot, unit);
-			snapshot.UnitIndex++;
-			if (snapshot.UnitIndex >= snapshot.UnitsQueue.MaxUnits)
-			{
-				snapshot.Finished = true;
-			}
-			else
-			{
-				var nextUnit = snapshot.Field.Spawn(snapshot.UnitsQueue.Get(snapshot.UnitIndex));
-				if (Collisions.GetCollision(snapshot.Field, nextUnit) != CollisionType.None)
-				{
-					snapshot.Finished = true;
-				}
-				else
-				{
-					snapshot.CurrentUnit = nextUnit;
-				}
-			}
-		}
+        private static void LockUnit(Snapshot snapshot, Unit unit)
+        {
+            foreach (var pos in unit.Members)
+            {
+                snapshot.Field[pos.X, pos.Y] = true; // lock position
+            }
+            DeleteLines(snapshot, unit);
+            snapshot.UnitIndex++;
+            if (snapshot.UnitIndex >= snapshot.UnitsQueue.MaxUnits)
+            {
+                snapshot.Finished = true;
+            }
+            else
+            {
+                var nextUnit = snapshot.Field.Spawn(snapshot.UnitsQueue.Get(snapshot.UnitIndex));
+                if (Collisions.GetCollision(snapshot.Field, nextUnit) != CollisionType.None)
+                {
+                    snapshot.Finished = true;
+                }
+                else
+                {
+                    snapshot.CurrentUnit = nextUnit;
+                }
+            }
+        }
 
-		// TODO: implement
-		private static Snapshot DeleteLines(Snapshot snapshot, Unit lockedUnit)
-		{
-			for (int i = lockedUnit.GetMinY(); i <= lockedUnit.GetMaxY(); i++)
-			{
-				bool lineFilled = snapshot.Field.IsLineFull(i);
-			}
-			// TODO: shift
-			// TODO: update score
-			// TOOD: save prev deleted lines to multiply score
-			return snapshot;
-		}
-	}
+        // TODO: implement
+        private static Snapshot DeleteLines(Snapshot snapshot, Unit lockedUnit)
+        {
+            var filled = new List<int>();
+            for (int i = lockedUnit.GetMinY(); i <= lockedUnit.GetMaxY(); i++)
+            {
+                if (snapshot.Field.IsLineFull(i))
+                    filled.Add(i);
+            }
+            if (filled.Any())
+            {
+                const int size = sizeof (byte);
+                var cleared = snapshot.Field
+                    .GetEnumerable()
+                    .Select((val, i) => new {val, i})
+                    .GroupBy((pair) => pair.i/size)
+                    .Where((group, i) => !filled.Contains(i))
+                    .SelectMany(group => group.Select(pair => pair.val));
+                snapshot.Field.SaveEnumerable(Enumerable.Repeat(true, filled.Count*size).Concat(cleared));
+            }
+            // TODO: update score
+            // TOOD: save prev deleted lines to multiply score
+            return snapshot;
+        }
+
+
+    }
 }
